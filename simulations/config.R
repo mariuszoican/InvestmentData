7d3da6b328f14476aa7afe59bc34d5634e68a35f
Marius Zoican <m.a.zoican@gmail.com>
2026-01-15 12:54:20 -0700

    Update config.R

--- simulations/config.R
+++ simulations/config.R
@@ -1,13 +1,13 @@
 # config.R
 
 config <- list(
-  mu        = 0.15,
-  sigma     = 0.12,
+  mu = 0.15,
+  sigma = 0.12,
   sigma_imb = 0.08,
-  w0        = 100,
-  gamma     = 24,
-  N_plot.   = 16,
+  w0 = 100,
+  gamma = 24,
+  N_plot. = 16,
   # defaults for simulation
-  N         = 50000,
-  inner     = 5000
+  N = 50000,
+  inner = 5000
 )
