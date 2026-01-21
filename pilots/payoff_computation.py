1a706615d91aee377baa3d866af0b202dc11d45a
Marius Zoican <m.a.zoican@gmail.com>
2026-01-20 21:36:16 -0700

    Add PAP and payoff

---
+++ pilots/payoff_computation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+post_name = "post_exp_2026-01-18"
+session_code = ["8aeypvgw"]  # Jan 17, desktop only
+
+data = pd.read_csv(f"{post_name}.csv")  # post experimental data
+# Keep only relevant sessions
+data = data[data["session.code"].isin(session_code)]
+# Keep only participants who finished
+data = data[data["participant._current_page_name"].isin(["FinalForProlific"])]
+
+data["final_payoff"] = data["player.payoff"] + data["player.payoff_for_quiz"]
